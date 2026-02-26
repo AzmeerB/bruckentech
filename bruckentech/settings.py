c9449b1dd815b9464a18be6e67b835894ec33073
Bruckentech Team <hello@bruckentech.org>
2026-02-26 20:04:58 +0300

    Set DEBUG to False by default for production

--- bruckentech/settings.py
+++ bruckentech/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.getenv(
 )
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv('DEBUG', 'True').lower() in ('true', '1', 'yes')
+DEBUG = os.getenv('DEBUG', 'False').lower() in ('true', '1', 'yes')
 
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost,127.0.0.1,bruckentech-ug.onrender.com').split(',')
 
